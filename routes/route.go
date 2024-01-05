e49252b79a288691e90011bae6b1e8c09b203fe6
Tomás Espinosa <40611089+TomGaleano@users.noreply.github.com>
2024-01-04 22:33:49 -0500

    Update route.go

--- routes/route.go
+++ routes/route.go
@@ -9,7 +9,7 @@ import (
 
 func Setup(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173, https://fictional-space-memory-9qrr64w7v67cgxg-5173.app.github.dev",
+		AllowOrigins:     "http://localhost:5173, https://fictional-space-memory-9qrr64w7v67cgxg.app.github.dev",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept",
 		AllowCredentials: true,
